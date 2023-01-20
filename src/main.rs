ca17ff0f12c9ffc1da25c3b31cc475d5e2e5d5cf
lumo03 <luis.moncada.mso@gmail.com>
2023-01-20 13:16:55 +0100

    improve code

--- src/main.rs
+++ src/main.rs
@@ -6,34 +6,37 @@ use std::{
 fn main() {
     let ip = "127.0.0.1";
     let port = "8080";
-    let stream = TcpStream::connect(format!("{}:{}", ip, port));
-    match stream {
-        Ok(mut stream) => {
-            println!("Connected");
-            loop {
-                let mut input = String::new();
-                io::stdin().read_line(&mut input).unwrap();
-                if input.trim() == "quit" {
+    // connect to the server
+    let mut stream =
+        TcpStream::connect(format!("{}:{}", ip, port)).expect("Couldn't connect to the server");
+    println!("Successfully connected to '{}:{}'!", ip, port);
+    loop {
+        // get input from user
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Error reading input");
+        // exit the loop if the user types "quit"
+        if input.trim() == "quit" {
+            break;
+        }
+        // write the input to the server
+        stream
+            .write_all(input.as_bytes())
+            .expect("Error writing to stream");
+        let mut read = [0; 1028];
+        match stream.read(&mut read) {
+            Ok(n) => {
+                if n == 0 {
                     break;
                 }
-                stream.write_all(input.as_bytes()).unwrap();
-
-                let mut read = [0; 1028];
-
-                match stream.read(&mut read) {
-                    Ok(n) => {
-                        if n == 0 {
-                            break;
-                        }
-                        let response = std::str::from_utf8(&read[0..n])
-                            .unwrap()
-                            .trim_end_matches('\n');
-                        println!("{}", response);
-                    }
-                    Err(_) => panic!("Error"),
-                }
+                // get the response from the server
+                let response = std::str::from_utf8(&read[0..n])
+                    .expect("Error reading response")
+                    .trim_end_matches('\n');
+                println!("{}", response);
             }
+            Err(_) => panic!("Error"),
         }
-        Err(_) => panic!("Couldn't connect to the server."),
     }
 }
